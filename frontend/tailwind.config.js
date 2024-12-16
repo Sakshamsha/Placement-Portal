85a46c007d4fd83b3d5ee8d03a6243538ef42bac
saksham sharma <130827315+Sakshamsha@users.noreply.github.com>
2024-12-16 12:36:07 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
 export const darkMode = ["class"];
 export const content = ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"];
 export const theme = {
@@ -52,4 +53,4 @@ export const theme = {
 		}
 	}
 };
-export const plugins = [require("tailwindcss-animate")];
+export const plugins = [tailwindcssAnimate];
