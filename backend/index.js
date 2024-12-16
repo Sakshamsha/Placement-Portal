8e66ebf8f3272439e5dddf56dd009a423bedf9dd
saksham sharma <130827315+Sakshamsha@users.noreply.github.com>
2024-12-16 13:57:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'*',
+    origin:'https://placement-portal-saksham.vercel.app',
     credentials:true
 }
 
